d394e45e8c1160d04c4213258c65ee12ac7ba58e
manideep <manideepperumalla@gmail.com>
2019-09-09 05:36:52 -0700

    9sep2019

---
+++ 9-sep/practical/print num in reverse.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace print num in reverse
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+         ?? a;
+      
+         a =10;
+         while (!(a<1))
+         {
+            Console.WriteLine(a);
+            a =a-1;
+         }
+      }
+   }
+}
